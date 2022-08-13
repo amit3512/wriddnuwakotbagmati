7d8801c0aa3823420d6c1f408b4629517a279258
amit3512 <ambadcr7@gmail.com>
2022-08-13 15:58:23 +0545

    fix

--- client/src/admin/Dashboard/StaffList.js
+++ client/src/admin/Dashboard/StaffList.js
@@ -1,88 +1,103 @@
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
-import { getStaff,deleteStaff} from "../../actions/adminActions";
+import { getStaff, deleteStaff } from "../../actions/adminActions";
 
+const StaffList = () => {
+  const dispatch = useDispatch();
+  const staffs = useSelector((state) => state?.staffs?.staffs);
+  console.log("staffs", staffs);
 
-const StaffList = () =>{
-
-const staffs = useSelector((state)=>state.staffs.staffs);
-const dispatch = useDispatch();
-
-const onDelete = (id) =>{
+  const onDelete = (id) => {
     dispatch(deleteStaff(id));
-}
+  };
 
-useEffect( () => {
-        dispatch(getStaff());
-},[]);
+  useEffect(() => {
+    if (!staffs) dispatch(getStaff());
+  }, [dispatch, staffs]);
 
-if (staffs === undefined) {
-    return <h4 alt="loading..." className="text-center mt-5"> Loading Staff... </h4>
+  if (!staffs) {
+    return (
+      <h4 alt="loading..." className="text-center mt-5">
+        {" "}
+        Loading Staff...{" "}
+      </h4>
+    );
   }
-    return(
+
+  return (
+    <div>
+      {!localStorage.getItem("userTokenTime") ? (
         <div>
-            {!localStorage.getItem("userTokenTime")?(
-                <div>
-                    <Redirect to="/signIn" />
-                </div>
-            ):(
-                <div className="container">
-                    <h4 className="text-center">Staffs</h4>
-                <div className="row">
-                
-                    <div className="col-md-10">
-                             <table className="table table-border">
-                             
-                                 <thead>
-                                     <tr>
-                                         <th>क्र.स</th>
-                                         <th>नाम</th>
-                                         <th>पद</th>
-                                         <th>फोन न.</th>
-                                         <th>ई-मेल</th>
-                                         <th></th>
-    
-                                     </tr>
-                                 </thead>
-                                    
-                                 {staffs && staffs.map((user,index)=>{
-                                        return (
-                                        <tbody>
-                                    
-                                            <tr key={index+1}>
-                                            <td>{user.orderBy}</td>
-                                            <td>{user.name}</td>
-                                            <td>{user.designation}</td>
-                                            <td>{user.phone}</td>
-                                            <td>{user.email}</td>
-                                             <td ><img  src={user.image} className="listing-images" alt="profile img"/></td>
-                                            {/* <td >{user.image.map((x,index)=><img key={index} src={x} className="profile_image" alt="profile img"/>)}</td> */}
-                                            <td className="EditDeleteButton">
-                                                <a href={`/staff/${user._id}`}><button className="btn btn-warning m-1">Edit</button></a>
-                                               <button className="btn btn-danger" onClick={()=>onDelete(`${user._id}`)}>Delete</button>
-                                            </td>
-        
-                                        </tr>
-                                    
-                                        </tbody>
-                                            )
-                                        })}
-                                 </table>
-                    </div>
-                    
-                    <div className="col-md-2">
-                            <div className="add-button">
-                                <a href="/admin/addStaff"><button className="btn btn-primary">Add Staff</button></a>
-                            </div>
-                    </div>
-                </div>
-            </div>
-             )} 
+          <Redirect to="/signIn" />
         </div>
-        
-    )
-}
+      ) : (
+        <div className="container">
+          <h4 className="text-center">Staffs</h4>
+          <div className="row">
+            <div className="col-md-10">
+              <table className="table table-border">
+                <thead>
+                  <tr>
+                    <th>क्र.स</th>
+                    <th>नाम</th>
+                    <th>पद</th>
+                    <th>फोन न.</th>
+                    <th>ई-मेल</th>
+                    <th></th>
+                  </tr>
+                </thead>
+
+                {staffs &&
+                  staffs.map((user, index) => {
+                    return (
+                      <tbody>
+                        <tr key={index + 1}>
+                          <td>{user.orderBy}</td>
+                          <td>{user.name}</td>
+                          <td>{user.designation}</td>
+                          <td>{user.phone}</td>
+                          <td>{user.email}</td>
+                          <td>
+                            <img
+                              src={user.image}
+                              className="listing-images"
+                              alt="profile img"
+                            />
+                          </td>
+                          {/* <td >{user.image.map((x,index)=><img key={index} src={x} className="profile_image" alt="profile img"/>)}</td> */}
+                          <td className="EditDeleteButton">
+                            <a href={`/staff/${user._id}`}>
+                              <button className="btn btn-warning m-1">
+                                Edit
+                              </button>
+                            </a>
+                            <button
+                              className="btn btn-danger"
+                              onClick={() => onDelete(`${user._id}`)}
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    );
+                  })}
+              </table>
+            </div>
+
+            <div className="col-md-2">
+              <div className="add-button">
+                <a href="/admin/addStaff">
+                  <button className="btn btn-primary">Add Staff</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default StaffList;
--- client/src/components/Staff.js
+++ client/src/components/Staff.js
@@ -1,61 +1,64 @@
 // import logo from '../images/staff.png';
-import {useEffect} from 'react';
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getStaff} from "../actions/adminActions";
+import { getStaff } from "../actions/adminActions";
 import Fade from "react-reveal";
 import Slide from "react-reveal/Slide";
 
 function Staff() {
+  const staffs = useSelector((state) => state?.staffs?.staffs);
+  const dispatch = useDispatch();
 
-    const staffs = useSelector((state)=>state.staffs.staffs);
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getStaff());
+  }, []);
 
-    useEffect( () => {
-         dispatch(getStaff());
- },[]);
-
-    if (staffs === undefined) {
-        return <h4 alt="loading..." className="text-center mt-5"> Loading Staffs... </h4>
-      }
+  if (staffs === undefined) {
     return (
-       <div className="Staff container">
-           <div>
-               <h4 className="text-center bg-light">कर्मचारीहरुको विवरण</h4>
-           </div>
-          <div>
-                 <div className="row">
-                     {staffs.map((staff,index)=>{
-                         return(
-                            <div className="col-md-4" key={index}>
-                            <div className="card">
-                                <Slide top>
-                                        <div className="staff-image">
-                                        <img className="card-img-top img-thumbnail" src={staff.image} alt="staff"/>
-                                        </div>
-                                </Slide>
-                               
-                                   <Fade left>
-                                   <div className="card-body">
-                                        <h5 className="card-title">{staff.name}</h5>
-                                        <p className="card-text">{staff.designation}</p>
-                                        <p className="card-text">{staff.phone}</p>
-                                        <p className="card-text">{staff.email}</p>
-                                        {/* <a href="/#" className="btn btn-primary">Go somewhere</a> */}
-                                    </div>
-                                   </Fade>
-                                  
-                            </div>
-                     </div>
-                         )
-                                        
-                     })}
-                    
-
-                 </div>
-          </div>
-      </div>
+      <h4 alt="loading..." className="text-center mt-5">
+        {" "}
+        Loading Staffs...{" "}
+      </h4>
     );
   }
-  
-  export default Staff;
-  
+  return (
+    <div className="Staff container">
+      <div>
+        <h4 className="text-center bg-light">कर्मचारीहरुको विवरण</h4>
+      </div>
+      <div>
+        <div className="row">
+          {staffs.map((staff, index) => {
+            return (
+              <div className="col-md-4" key={index}>
+                <div className="card">
+                  <Slide top>
+                    <div className="staff-image">
+                      <img
+                        className="card-img-top img-thumbnail"
+                        src={staff.image}
+                        alt="staff"
+                      />
+                    </div>
+                  </Slide>
+
+                  <Fade left>
+                    <div className="card-body">
+                      <h5 className="card-title">{staff.name}</h5>
+                      <p className="card-text">{staff.designation}</p>
+                      <p className="card-text">{staff.phone}</p>
+                      <p className="card-text">{staff.email}</p>
+                      {/* <a href="/#" className="btn btn-primary">Go somewhere</a> */}
+                    </div>
+                  </Fade>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Staff;
